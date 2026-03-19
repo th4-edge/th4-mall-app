1a145b1a00fd1d52228207b2308027ca4dca9570
zhangwei <nealwinter@qq.com>
2026-03-19 13:50:02 +0800

    --搭建基础项目工程,uni-app,vue3,ts

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+onLaunch(() => {
+  console.log("App Launch");
+  uni.getSystemInfo().then((res) => {
+    console.log(res);
+  })
+  uni.login().then((res) => {
+    console.log(res);
+  })
+});
+onShow(() => {
+  console.log("App Show");
+});
+onHide(() => {
+  console.log("App Hide");
+});
+</script>
+<style></style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <view class="content">
+    <swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
+            :duration="duration">
+      <swiper-item>
+        <view class="swiper-item uni-bg-red">A</view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item uni-bg-green">B</view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item uni-bg-blue">C</view>
+      </swiper-item>
+    </swiper>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const indicatorDots = ref(true)
+const autoplay = ref(true)
+const interval = ref(5000)
+const duration = ref(500)
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.swiper {
+  width: 100%;
+  height: 560rpx;
+}
+.swiper-item {
+  display: block;
+  height: 560rpx;
+  line-height: 560rpx;
+  text-align: center;
+}
+.swiper-list {
+  margin-top: 40rpx;
+  margin-bottom: 0;
+}
+.uni-common-mt {
+  margin-top: 60rpx;
+  position: relative;
+}
+.info {
+  position: absolute;
+  right: 20rpx;
+}
+.uni-padding-wrap {
+  width: 550rpx;
+  padding: 0 100rpx;
+}
+.uni-bg-blue {
+  background-color: blue;
+}
+.uni-bg-green {
+  background-color: green;
+}
+.uni-bg-red {
+  background-color: red;
+}
+</style>
